c56f95d452ea6c64b14f65efa56b75c3de22a69f
TWetmore34 <tjwetmore34@gmail.com>
2022-06-26 15:03:28 -0400

    changed the question display function to make for easier question additions

--- js/script.js
+++ js/script.js
@@ -6,127 +6,60 @@ let answersEl = document.querySelectorAll('.answer')
 let startEl = document.getElementById('start')
 let inputEl = document.getElementsByName('answer')
 
+let time = 60;
 
 
 // Make sure to save these to browser info and add a reset button on home menu
 let correct = 0;
 
-let questionMap = {
-    q1: ["What data type has a value of true or false?", 'strings', 'booleans', 'integers', 'numbers'],
-    q2: ['What is an object?', 'a collection of strings', 'a for in loop to display data', 'a standalone entity with properties and a type', 'the contents of the whole browser'],
-    q3: ['pretend this is a question', 'wrong', 'wrong again', 'are you even trying?', 'Pick me!'],
-    q4: ['Wow this is a really hard one! Can you figure it out?', 'please dont look at me', 'free me from this prision of flesh you call a laptop', 'sodie pap', 'this is the right answer again.']
-};
+let questionArr = [["What data type has a value of true or false?", 'strings', 'booleans', 'integers', 'numbers', function(){
+    inputEl[1].setAttribute('value', 'true');
+}], ['What is an object?', 'a collection of strings', 'a for in loop to display data', 'a standalone entity with properties and a type', 'the contents of the whole browser', function(){
+    inputEl[2].setAttribute('value', 'true');
+}], ['pretend this is a question?', 'wrong', 'wrong again', 'are you even trying?', 'Pick me!', function(){
+    inputEl[3].setAttribute('value', 'true');
+}], ['Wow this is a really hard one! Can you figure it out?', 'please dont look at me', 'free me from this prision of flesh you call a laptop', 'sodie pap', 'this is the right answer again.', function(){
+    inputEl[3].setAttribute('value', 'true');
+}]
+]
 
-function question1 (event) {
-    // Display
-    event.preventDefault();
-    questionEl.textContent = questionMap.q1[0];
-    for (let i=0;i<questionMap.q1.length-1;i++){
-        answersEl[i].innerHTML = questionMap.q1[i+1];
+function testArr(num) {
+    questionEl.innerHTML = questionArr[num][0];
+    for(i=0;i<questionArr[num].length-2;i++){
+        answersEl[i].innerHTML = questionArr[num][i+1];
+        inputEl[i].removeAttribute('value');
+        questionArr[num][5]();
     }
-    // replace start w/ submit
     startEl.innerHTML = "submit"
     startEl.setAttribute('id', 'submit');
+    let submitEl = document.getElementById('submit');
 
-    // reaccess replaced button
-    let submitEl = document.getElementById('submit')
+    submitEl.addEventListener('click', function(e){
+        e.preventDefault()
+        
+        
+        console.log(inputEl[0].value)
+        console.log(inputEl[1].value)
+        console.log(inputEl[2].value)
+        console.log(inputEl[3].value)
+        
+        // the for loop here is causing trouble. it loops thru the for loop presenting alerts several times over for each question. could i change it to search through the array inputEl for a particular .value?
+        for(i=0;i<inputEl.length;i++) {
+        if (inputEl[i].checked){
+            if(inputEl[i].value == 'true'){
+                correct += 1;
+                alert('correct!')
+            } else {alert('better luck next time')}
+        }}
+        if(time === 0 || num > questionArr.length) {
+            return alert('thanks for playing! Your score: ' + correct / questionArr.length)
+        } else {testArr(num + 1)}
 
-    // checks for selection and true value
-    submitEl.addEventListener('click', function(){
-
-        // checks for checked attribute on correct answer
-            if (inputEl[1].checked){
-                    correct += 1;
-                    alert("congrats, you got it right")
-            } else {
-                    
-                    alert("sorry, not this time :/")
-                }
-            // question 2 function 
-            question2()
-        }
-    )
+    })
+    
 };
 
-function question2() {
-     // Display
-    questionEl.textContent = questionMap.q2[0];
-    for (let i=0;i<questionMap.q2.length-1;i++){
-        answersEl[i].innerHTML = questionMap.q2[i+1];
-    }
-    // redeclare access for button
-    let submitEl = document.getElementById('submit')
-
-    // checks for selection
-    submitEl.addEventListener('click', function(){
-
-        // checks for checked attribute on correct answer
-            if (inputEl[2].checked){
-                    correct += 1;
-                    alert("congrats, you got it right")
-            } else {
-                    alert("sorry, not this time :/")
-                }
-            // question 3 
-            question3();
-        }
-    )
-};
-
-function question3() {
-    // Display
-   event.preventDefault();
-   questionEl.textContent = questionMap.q3[0];
-   for (let i=0;i<questionMap.q3.length-1;i++){
-       answersEl[i].innerHTML = questionMap.q3[i+1];
-   }
-//    redeclare button access
-   let submitEl = document.getElementById('submit')
-
-   // checks for selection and true value
-   submitEl.addEventListener('click', function(){
-
-       // checks for checked attribute on correct answer
-           if (inputEl[3].checked){
-                   correct += 1;
-                   alert("congrats, you got it right")
-           } else {
-                   alert("sorry, not this time :/")
-               }
-           // question 4
-           question4();
-       }
-   )
-};
-
-function question4() {
-    // Display
-   event.preventDefault();
-   questionEl.textContent = questionMap.q4[0];
-   for (let i=0;i<questionMap.q4.length-1;i++){
-       answersEl[i].innerHTML = questionMap.q4[i+1];
-   }
-//    redeclare button
-   let submitEl = document.getElementById('submit')
-
-   // checks for selection and true value
-   submitEl.addEventListener('click', function(){
-
-       // checks for checked attribute on correct answer
-           if (inputEl[3].checked){
-                   correct += 1;
-                   alert("congrats, you got it right")
-           } else {
-                   alert("sorry, not this time :/")
-               }
-            alert("final score: " + correct / 4);
-           return;
-       }
-   )
-};
-
-startEl.addEventListener('click', question1)
+testArr(0);
 
 // Also, just steal the timer funciton we have on the other game
 // function for that argment takes argument of num where num on start = 0
